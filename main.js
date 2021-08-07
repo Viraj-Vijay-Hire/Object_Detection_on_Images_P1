e366fefc37e8c9f4d25ac0970fc2c39cf7e4b1f5
Viraj-Vijay-Hire <78733888+Viraj-Vijay-Hire@users.noreply.github.com>
2021-08-07 17:20:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,8 +4,11 @@ objects = [];
 
 function setup()
 {
-    canvas = createCanvas(640, 420);
+    canvas = createCanvas(380, 380);
     canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
 
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
@@ -15,7 +18,7 @@ function modelLoaded()
 { 
     console.log("Model is Loaded");
     status = true;
-    objectDetector.detect(img, gotResult);
+    objectDetector.detect(video, gotResult);
 }
 
 function gotResult(error, results)
@@ -38,7 +41,7 @@ function draw()
 
     ///Rectangle for dog///
 
-    image(img, 0, 0, 640, 420);
+    image(video, 0, 0, 380, 380);
     /**
     fill('#65088e');
     text("dog", 150, 75);
@@ -56,17 +59,26 @@ function draw()
 
     ///End of making rects///
 */
+
     if(status != "")
     {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+
+        objectDetector.detect(video, gotResult);
+
         for(i = 0; i < objects.length; i++)
         {
             document.getElementById("status").innerHTML = "Status: Object Detected";
 
-            fill("#ff0000");
+            document.getElementById("number_of_object").innerHTML = "Number of Objects Detected are : " + objects.length;
+
+            fill(r, g, b);
             percent = floor(objects[i].confidence * 100);
             text(objects[i].label + " " + percent + "% ", objects[i].x + 100, objects[i].y);
             noFill();
-            stroke("#ff0000");
+            stroke(r, g, b);
             rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
         }
     }
